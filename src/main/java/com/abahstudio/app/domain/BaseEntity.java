392d93ebbf4c938139133fa1918bee513a7d02ac
Dede Lukmanul Hakim <ddlukmanulhakim@gmail.com>
2025-12-09 22:30:12 +0700

    add base entity

---
+++ src/main/java/com/abahstudio/app/domain/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.abahstudio.app.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter @Setter
+public abstract class BaseEntity {
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
+
+    private LocalDateTime deletedAt; // null = active
+
+    @Version
+    private Long version;
+}
