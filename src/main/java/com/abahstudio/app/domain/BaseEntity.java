f832be62ec6e8e13f8796f6cc0f13e682d94d1b4
Dede Lukmanul Hakim <ddlukmanulhakim@gmail.com>
2025-12-12 09:18:49 +0700

    update base entity automatic created By , updated By, safe delete

--- src/main/java/com/abahstudio/app/domain/BaseEntity.java
+++ src/main/java/com/abahstudio/app/domain/BaseEntity.java
@@ -1,16 +1,24 @@
 package com.abahstudio.app.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Version;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.annotations.UpdateTimestamp;
-
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
 import java.time.LocalDateTime;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@SQLDelete(sql = "UPDATE {h-schema}{h-table} SET deleted_at = now() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
 @Getter @Setter
 public abstract class BaseEntity {
 
@@ -21,8 +29,11 @@ public abstract class BaseEntity {
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
+    @CreatedBy
+    @Column(updatable = false)
     private String createdBy;
 
+    @LastModifiedBy
     private String updatedBy;
 
     private LocalDateTime deletedAt; // null = active
